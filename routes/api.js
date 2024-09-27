03801618768508e5b10fc8cc3a4523b5d307bb55
Santiago Olguín <SantoII1735@gmail.com>
2024-09-27 16:06:48 +0000

    created issueSchema

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,25 @@
 const mongoose = require('mongoose');
-
-mongoose.connect(process.env.MONGO_URI);
+const express = require('express');
 
 'use strict';
 
+mongoose.connect(process.env.MONGO_URI);
+
+const issueSchema = new mongoose.Schema({
+  issue_title: {type: String, required: true},
+  issue_text: {type: String, required: true},
+  created_on: {type: Date, required: true},
+  updated_on: {type: Date, required: true},
+  created_by: {type: String, required: true},
+  assigned_to: {type: String, required: false},
+  open: {type: Boolean, required: true},
+  status_text: {type: String, required: false},
+  project_name : {type: String, required: true}
+});
+
 module.exports = function (app) {
+
+  app.use(express.urlencoded({ extended: true }))
 
   app.route('/api/issues/:project')
   
