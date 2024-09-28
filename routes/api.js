f6871a4bea52f5999b8cf86a3293488262870827
Santiago Olguín <SantoII1735@gmail.com>
2024-09-28 20:33:24 +0000

    added post to /api/issues/:project

--- routes/api.js
+++ routes/api.js
@@ -11,9 +11,9 @@ const issueSchema = new mongoose.Schema({
   created_on: {type: Date, required: true},
   updated_on: {type: Date, required: true},
   created_by: {type: String, required: true},
-  assigned_to: {type: String, required: false},
+  assigned_to: {type: String, required: true},
   open: {type: Boolean, required: true},
-  status_text: {type: String, required: false},
+  status_text: {type: String, required: true},
   project_name : {type: String, required: true}
 });
 
@@ -31,8 +31,32 @@ module.exports = function (app) {
     })
     
     .post(function (req, res){
-      let project = req.params.project;
+      let now = new Date();
+      console.log(`Received Post request with following parameters:`);
+      console.log(req.params.project);
+      console.log(req.body);
       
+      // make sure necessary params are present
+      if ( !(req.body.issue_title && req.body.issue_text && req.body.created_by) ) {
+        res.json({
+          error: 'required field(s) missing'
+        })
+      }
+
+      const newIssue = new Issue({
+        issue_title: req.body.issue_title,
+        issue_text: req.body.issue_text,
+        created_on: now,
+        updated_on: now,
+        created_by: req.body.created_by,
+        assigned_to: req.body.assigned_to || "",
+        open: true,
+        status_text: req.body.status_text || "",
+        project_name: req.params.project
+      })
+
+      newIssue.save();
+      res.json(newIssue);
     })
     
     .put(function (req, res){
